332915d0df5022580a49267ecf653192d0e37fef
LiFaytheGoblin <lifaythegoblin@mailbox.org>
2025-09-08 12:01:49 +0200

    select different sentence structure

--- generator.py
+++ generator.py
@@ -1,7 +1,11 @@
 import random
 
 def get_poem_line(nouns, verbs, adverbs): 
-    return f"Let {random.choice(nouns)} {random.choice(verbs)} {random.choice(adverbs)}"
+    sentence_structure = random.randint(0,1)
+    if (sentence_structure == 0): 
+        return f"Let {random.choice(nouns)} {random.choice(verbs)} {random.choice(adverbs)}."
+    else:
+        return f"Why would {random.choice(nouns)} not {random.choice(verbs)} {random.choice(adverbs)}?"
 
 def main():
     nouns=["flowers", "rain", "trees", "grass"]
